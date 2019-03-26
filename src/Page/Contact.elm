7b849c3261c6c9bcd95903e1d839f9cf60303121
k-nasa <htilcs1115@gmail.com>
2019-03-26 23:02:09 +0900

    add contact icon

--- src/Page/Contact.elm
+++ src/Page/Contact.elm
@@ -7,4 +7,31 @@ import Html.Attributes exposing (..)
 view : Html msg
 view =
     div [ id "page-contact" ]
-        [ h1 [] [ text "Contact" ] ]
+        [ h1 [] [ text "Contact" ]
+        , p [] [ text "何かあればTwitterまでご連絡ください。" ]
+        , ul [ class "links" ]
+            [ li []
+                [ a [ href "https://twitter.com/nasa_desu" ]
+                    [ i [ class "fab fa-twitter" ] []
+                    ]
+                ]
+            , li
+                []
+                [ a [ href "https://github.com/k-nasa" ]
+                    [ i [ class "fab fa-github" ] []
+                    ]
+                ]
+            , li
+                []
+                [ a [ href "https://nasaemon.hateblo.jp/" ]
+                    [ i [ class "fas fa-edit" ] []
+                    ]
+                ]
+            , li
+                []
+                [ a [ href "https://www.facebook.com/nasa.x68" ]
+                    [ i [ class "fab fab fa-facebook-f" ] []
+                    ]
+                ]
+            ]
+        ]
